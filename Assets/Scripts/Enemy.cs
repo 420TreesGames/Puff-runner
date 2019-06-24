38224ba5c39f8af66ceb9aef9472d94111821ca5
420treesgames <420trees.games@gmail.com>
2019-06-24 23:07:24 +0200

    24.06.19

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
-
+	[SerializeField] Sprite[] sprite;
+	Image image;
 	background groundSpeed;
 	float speed;
 	Rigidbody2D rb;
 
 	void Start () {
+		image=GetComponent<Image>();
 		groundSpeed=GameObject.FindGameObjectWithTag("Ground").GetComponent<background>();
 		rb=GetComponent<Rigidbody2D>();
+		RandomSprite();
+
 	}
 
 	void Update () {
 		speed=groundSpeed.speed;
 		rb.velocity= Vector2.right*speed;		
 		
+	}
+	void RandomSprite(){
+		int random = Random.Range(0,sprite.Length);
+		image.sprite=sprite[random];
 	}
 }
